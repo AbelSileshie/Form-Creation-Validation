37c69535a06aac894b88d17c0e2cb2e4011cfe88
Abelsileshie <sileshieabel@gmail.com>
2024-10-11 14:25:30 +0300

    last-edit with details

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
-  feedbackDiv.style.display = "block";
-
   form.addEventListener("submit", async (event) => {
+    function errormessage(A) {
+      feedbackDiv.innerHTML = errormessages[A];
+      feedbackDiv.style.color = "#dc3545";
+      isvalid = false;
+    }
+    let isvalid = true;
+    if (isvalid === true) {
+      feedbackDiv.style.display = "block";
+      feedbackDiv.style.color = "#28a745";
+    } else {
+      feedbackDiv.style.display = "Block";
+    }
     event.preventDefault();
     const username = form.username.value.trim();
     const password = form.password.value.trim();
@@ -13,23 +23,15 @@ document.addEventListener("DOMContentLoaded", () => {
       "Please Enter A strong Password it must be over 8 character",
       "Please enter A valid Username",
     ];
-    let isvalid = true;
-    if (!email.includes("@") || !email.includes(".")) {
-      feedbackDiv.innerHTML = errormessages[0];
-      isvalid = false;
-      console.log(errormessages[0]);
+    if (!email.includes("@gmail") || !email.includes(".")) {
+      errormessage(0);
     } else if (password.length <= 8) {
-      feedbackDiv.innerHTML = errormessages[1];
-      isvalid = false;
-      console.log(errormessages[1]);
+      errormessage(1);
     } else if (username.length < 3) {
-      feedbackDiv.innerHTML = errormessages[2];
-      isvalid = false;
-      console.log(errormessages[2]);
+      errormessage(2);
     } else {
-      feedbackDiv.innerHTML = "Success";
-      console.log("Success");
-      console.log(username, password, email);
+      feedbackDiv.innerHTML =
+        "<br> Congrats you have been registered Check your email";
     }
   });
 });
